f68ee95ade5b572179bc03cfdba98960e2b5c494
GregDevLab <g.batte@outlook.fr>
2024-08-17 16:40:03 +0200

    premier method controller

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+	@Get()
+	getAll() {
+
+	}
+	
+	@Get('/:id')
+	getOne(@Param('id') id: string, @Res() res: Response) {
+		console.log("🚀 ~ MessagesController ~ getOne ~ id:", id)
+		return res.status(HttpStatus.CREATED).json(id)
+	}
+
+	@Post()
+	create(@Body() body : any, @Res() res: Response) {
+		console.log(body)
+		return res.status(HttpStatus.CREATED).json(body)
+	}
+
+}
