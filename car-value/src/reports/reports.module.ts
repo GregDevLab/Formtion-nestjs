38bb61123d5acfaa256d851dbb4f1d74a53dd47b
GregDevLab <g.batte@outlook.fr>
2024-08-19 16:24:38 +0200

    mise en place report

--- car-value/src/reports/reports.module.ts
+++ car-value/src/reports/reports.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 import { ReportsRepository } from './reports.reopsitory';
+import { PrismaService } from 'src/prisma.service';
+import { PrismaModule } from 'src/Prisma.module';
 
 @Module({
 	controllers: [ReportsController],
 	providers: [
 		ReportsService,
 		ReportsRepository
-	]
+	],
+	imports: [PrismaModule]
 })
 export class ReportsModule {}
--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	constructor(
+		private readonly userService: UsersService,
+	) {}
+
+	@Get('/:id')
+	async getUser(@Param('id') id: string): Promise<User>{
+		return await this.userService.getUniqueUser({id: Number(id)})
+	}
+
+}
