0b78b07b277fd6f103a589fb892ec0ca0e0d28a4
GregDevLab <g.batte@outlook.fr>
2024-08-19 10:29:24 +0200

    partage du PowerService avec le CpuModule pour une utilisation dans le CpuService

--- injection-dependances/src/cpu/cpu.service.ts
+++ injection-dependances/src/cpu/cpu.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
+
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+	constructor(private powerService: PowerService) {}
+
+	compute(a:number, b:number) {
+		console.log('simulation : l\'addition consomme 10 watts')
+		this.powerService.supplyPower(10)
+		return a+b
+	}
+}
